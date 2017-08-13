2c03d00e9d797bc9432732694021daf2005298a0
Sreenu <thalari.srinu@gmail.com>
2017-08-13 17:40:55 +0530

    completed the interface theme

--- onlineshopping/src/main/java/com/sreenu/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/sreenu/onlineshopping/controller/PageController.java
@@ -10,32 +10,25 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "WELCOME TO SPRING MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
-
-	/*@RequestMapping(value = "/test")
-	public ModelAndView test(@RequestParam(value = "greeting", required = false) String greeting) {
-		if (greeting == null) {
-			greeting = "Welcome to Sreenu";
-		}
+ 
+	@RequestMapping(value ="/about")
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout",true);
 		return mv;
-
 	}
-*/
-	/*
-	@RequestMapping(value = "/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting") String greeting) {
-		if (greeting == null) {
-			greeting = "Welcome to Sreenu";
-		}
+	
+	@RequestMapping(value ="/contact")
+	public ModelAndView contact() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact",true);
 		return mv;
-
 	}
 
-	*/
 }
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/about.jsp
@@ -0,0 +1,16 @@
+<div class="container">
+
+<div class="row">
+<h1>About Us</h1>
+<hr>
+<p>
+INFO: Mapped URL path [/resources/**] onto handler 'org.springframework.web.servlet.resource.ResourceHttpRequestHandler#0'
+Aug 13, 2017 3:34:52 PM org.springframework.web.servlet.DispatcherServlet initServletBean
+INFO: FrameworkServlet 'dispatcher': initialization completed in 1520 ms
+Aug 13, 2017 3:48:44 PM org.springframework.web.servlet.PageNotFound noHandlerFound
+WARNING: No mapping found 
+
+</div>
+
+
+</div>
